8177b3d347743a474dd6189680b45917d6aeace5
Neo <DavidEddlemon@Gmail.com>
2016-10-18 16:26:08 -0500

    Silence stuff

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -731,7 +731,7 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
-	pr_info("%s: ctrl=%p ndx=%d\n", __func__, ctrl, ctrl->ndx);
+	pr_debug("%s: ctrl=%p ndx=%d\n", __func__, ctrl, ctrl->ndx);
 
 	if (pinfo->dcs_cmd_by_left) {
 		if (ctrl->ndx != DSI_CTRL_LEFT)
@@ -771,7 +771,7 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		ctrl->panel_data.panel_info.hbm_off_state = 0;
 	}
 
-	pr_info("%s-. Pwr_mode(0x0A) = 0x%x\n", __func__, pwr_mode);
+	pr_debug("%s-. Pwr_mode(0x0A) = 0x%x\n", __func__, pwr_mode);
 end:
 	pinfo->blank_state = MDSS_PANEL_BLANK_UNBLANK;
 	return 0;
@@ -791,7 +791,7 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
-	pr_info("%s: ctrl=%p ndx=%d\n", __func__, ctrl, ctrl->ndx);
+	pr_debug("%s: ctrl=%p ndx=%d\n", __func__, ctrl, ctrl->ndx);
 
 	if (pinfo->dcs_cmd_by_left) {
 		if (ctrl->ndx != DSI_CTRL_LEFT)
